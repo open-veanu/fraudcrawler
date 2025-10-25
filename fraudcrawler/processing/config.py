a818d00f7be144e91562ec7e35539c9791aba19b
chriguBERTO <christoph.jaeggli@gmail.com>
2025-10-25 13:50:38 +0200

    Added config files

---
+++ fraudcrawler/processing/config.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel, Field
+from typing import List
+
+from fraudcrawler import Prompt
+
+class ProcessingConfig(BaseModel):
+    """Sets up the processing pipeline step."""
+    prompts: List[Prompt] = Field(description="The list of prompt to use for classification.")
