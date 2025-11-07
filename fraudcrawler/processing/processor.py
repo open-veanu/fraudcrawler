4dab9252534d299cb705ce852742ca19dfc5633f
chriguBERTO <christoph.jaeggli@gmail.com>
2025-11-07 22:15:51 +0100

    Still not at a good point

--- fraudcrawler/processing/processor.py
+++ fraudcrawler/processing/processor.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractmethod
-from copy import deepcopy
 import logging
-from typing import Any, Dict, Sequence
+from typing import Any, Dict, List
 
 import httpx
 from openai import AsyncOpenAI
@@ -18,109 +17,184 @@ from fraudcrawler.settings import (
 logger = logging.getLogger(__name__)
 
 
-class Node(ABC):
-    """Defines an node inside a processing workflow."""
+class ClassificationWorkflow(ABC):
+    """Abstract base class for independant classification workflows."""
+
 
     def __init__(
             self,
             name: str,
         ):
-        """Base class of a processing node.
+        """Abstract base class for defining a classification workflow.
         
         Args:
-            name: Name of the node (unique identifier)
+            name: Name of the classification workflow.
         """
-        self.name = name
+        self._name = name
 
     @abstractmethod
     async def _run(
             self,
-            product_item: ProductItem,
+            product: ProductItem,
             user_input: Dict[str, Any] | None = None,
-            data: Dict[str, Any] | None = None
-        ) -> Dict[str, Any]:
+        ) -> ClassificationResult:
+        """Runs the classification.
+
+        Args:
+            product: Product item to process.
+            user_input: Additional user input data.
+        """
         pass
 
     async def run(
             self,
-            product_item: ProductItem,
+            product: ProductItem,
             user_input: Dict[str, Any] | None = None,
-            data: Dict[str, Any] | None = None
-        ) -> Dict[str, Any]:
-        """Runs the node by including and returning string specific data in addition to the product item."""
-        logger.debug(f'Start running node {self.name}')
+        ) -> ClassificationResult:
+        """Runs the classification and writes it to the product item.
+        
+        Args:
+            product: Product item to process.
+            user_input: Additional user input data.
+        """
+        logger.info(f'Start classification workflow {self._name}')
         try:
-            data_out = await self._run(product_item=product_item, user_input=user_input, data=data)
+            classification = await self._run(product=product, user_input=user_input)
         except Exception as e:
-            raise type(e)(f'(node={self.name}) {e}') from e
-        logger.debug(f'Run of node {self.name} finished successfully.')
-        return data_out
+            msg = f'Error while running classification workflow "{self._name}": {e}'
+            logger.error(msg=msg)
+            raise type(e)(msg) from e
+        logger.info(f'Run of node {self._name} finished successfully.')
+        return classification
 
 
-class OpenAIChat(Node):
+class OpenAIChat(ClassificationWorkflow):
     """Open AI Chat model."""
 
     _user_prompt_template = "Product Details:\n{product_details}\n\nRelevance:"
     _product_details_template = "{field_name}:\n{field_value}"
+    _default_allowed_classes = [0, 1]
+    _max_tokens = 1
 
     def __init__(
             self,
-            http_client: httpx.AsyncClient,
             name: str,
+            http_client: httpx.AsyncClient,
             api_key: str,
             model: str,
-
+            system_prompt: str,
+            product_item_fields: List[str],
+            allowed_classes: List[int] | None = None,
         ):
         """Open AI Chat node.
 
         Args:
             name: Name of the node (unique identifier)
-            status: Status for orchestrating node execution.
             http_client: An httpx.AsyncClient to use for the async requests.
             api_key: The OpenAI API key.
             model: The OpenAI model to use.
+            system_prompt: System prompt for the AI model.
+            product_item_fields: Product item fields used to construct the user prompt.
+            allowed_classes: Allowed classification values.
         """
         super().__init__(name=name)
         self._client = AsyncOpenAI(http_client=http_client, api_key=api_key)
         self._model = model
+        self._system_prompt = system_prompt
+        self._product_item_fields = product_item_fields
+        self._allowed_classes = allowed_classes if allowed_classes is not None else self._default_allowed_classes
+
+    @staticmethod
+    def _log_before(url: str, prompt: Prompt, retry_state: RetryCallState) -> None:
+        """Context aware logging before the request is made."""
+        if retry_state:
+            logger.debug(
+                f"Classifying product with url={url} using prompt={prompt.name} (Attempt {retry_state.attempt_number})."
+            )
+        else:
+            logger.debug(f"retry_state is {retry_state}; not logging before.")
+
+    @staticmethod
+    def _log_before_sleep(
+        url: str, prompt: Prompt, retry_state: RetryCallState
+    ) -> None:
+        """Context aware logging before sleeping after a failed request."""
+        if retry_state and retry_state.outcome:
+            logger.warning(
+                f"Attempt {retry_state.attempt_number} of classifying product with url={url} using prompt={prompt.name} "
+                f"failed with error: {retry_state.outcome.exception()}. "
+                f"Retrying in {retry_state.upcoming_sleep:.0f} seconds."
+            )
+
+    def _get_product_details(self, product: ProductItem) -> str:
+        """Extracts product details based on the configuration.
+
+        Args:
+            product: The product item to extract details from.
+        """
+        details = []
+        for name in self._product_item_fields:
+            if value := getattr(product, name, None):
+                details.append(
+                    self._product_details_template.format(
+                        field_name=name, field_value=value
+                    )
+                )
+            else:
+                logger.warning(
+                    f'Field "{name}" is missing in ProductItem with url="{product.url}"'
+                )
+        return "\n\n".join(details)
 
     async def _run(
             self,
-            product_item: ProductItem,
+            product: ProductItem,
             user_input: Dict[str, Any] | None = None,
-            data: Dict[str, Any] | None = None
-        ) -> Dict[str, Any]:
+        ) -> ClassificationResult:
+        """Calls the OpenAI API with the user prompt from the product."""
+
+        # Form the product details from the ProductItem
+        product_details = self._get_product_details(product=product)
+        if not product_details:
+            logger.warning("Missing required product_details for classification.")
+            return self._error_response
+
+        # Prepare the user prompt
+        user_prompt = PROCESSOR_USER_PROMPT_TEMPLATE.format(
+            product_details=product_details,
+        )
 
 
+        response = await self._client.chat.completions.create(
+            model=self._model,
+            messages=[
+                {"role": "system", "content": self._system_prompt},
+                {"role": "user", "content": user_prompt},
+            ],
+            max_tokens=self._max_tokens,
+        )
+        if not response or not (content := response.choices[0].message.content):
+            raise ValueError(
+                f'Error calling OpenAI API or empty response="{response}".'
+            )
 
-class ProcessingWorkflow(ABC):
-    """Class for representing an independant processing workflow."""
-
-    def __init__(self, name: str, nodes: Sequence[Node]):
-        self._name = name
-        self._nodes = nodes
-
-    async def run(self, product_item: ProductItem, user_input: Dict[str, Any] | None = None) -> Dict[str, Any]:
-        """Sequentially runs the nodes in the given order.
-        
-        Args:
-            product_item: product item to process.
-            user_input: additional user input data.
-        """
-        logger.info(f'Start processing workflow "{self._name}"')
-        data: Dict[str, Any] =  {}
+        # Convert the content to an integer
         try:
-            for node in self._nodes:
-                data = await node.run(product_item=product_item, user_input=user_input, data=data)
+            content = int(content.strip())
         except Exception as e:
-            msg = f'Error while running processing workflow "{self._name}": {e}'
-            logger.error(msg)
-            raise type(e)(msg) from e
-        return data
+            raise type(e)(f"Failed to convert OpenAI response '{content}' to integer: {e}") from e
 
+        # For tracking consumption we alre return the tokens used
+        classification = ClassificationResult(
+            result=content,
+            input_tokens=response.usage.prompt_tokens,
+            output_tokens=response.usage.completion_tokens,
+        )
+
+        return classification
 
 class Processor:
-    """Processes product data for classification based on a prompt configuration."""
+    """Processes product items for a set of classification workflows."""
 
     def __init__(
         self,
@@ -147,86 +221,17 @@ class Processor:
             output_tokens=empty_token_count,
         )
 
-    @staticmethod
-    def _get_product_details(product: ProductItem, prompt: Prompt) -> str:
-        """Extracts product details based on the prompt configuration.
-
+    async def run(self, product: ProductItem) -> ProductItem:
+        """Run the processing step for multiple classification workflows.
+        
         Args:
-            product: The product item to extract details from.
-            prompt: The prompt configuration containing field names.
+            product: The product item to run the classification workflows for.
         """
-        details = []
-        for field in prompt.product_item_fields:
-            if value := getattr(product, field, None):
-                details.append(
-                    PROCESSOR_PRODUCT_DETAILS_TEMPLATE.format(
-                        field_name=field, field_value=value
-                    )
-                )
-            else:
-                logger.warning(
-                    f'Field "{field}" is missing in ProductItem with url="{product.url}"'
-                )
-        return "\n\n".join(details)
+        for workflow in self._classification_workflows:
+            try:
+                workflow.run()
+            except:
 
-    @staticmethod
-    def _log_before(url: str, prompt: Prompt, retry_state: RetryCallState) -> None:
-        """Context aware logging before the request is made."""
-        if retry_state:
-            logger.debug(
-                f"Classifying product with url={url} using prompt={prompt.name} (Attempt {retry_state.attempt_number})."
-            )
-        else:
-            logger.debug(f"retry_state is {retry_state}; not logging before.")
-
-    @staticmethod
-    def _log_before_sleep(
-        url: str, prompt: Prompt, retry_state: RetryCallState
-    ) -> None:
-        """Context aware logging before sleeping after a failed request."""
-        if retry_state and retry_state.outcome:
-            logger.warning(
-                f"Attempt {retry_state.attempt_number} of classifying product with url={url} using prompt={prompt.name} "
-                f"failed with error: {retry_state.outcome.exception()}. "
-                f"Retrying in {retry_state.upcoming_sleep:.0f} seconds."
-            )
-
-    async def _call_openai_api(
-        self,
-        system_prompt: str,
-        user_prompt: str,
-        **kwargs,
-    ) -> ClassificationResult:
-        """Calls the OpenAI API with the given user prompt."""
-        response = await self._client.chat.completions.create(
-            model=self._model,
-            messages=[
-                {"role": "system", "content": system_prompt},
-                {"role": "user", "content": user_prompt},
-            ],
-            **kwargs,
-        )
-        if not response or not (content := response.choices[0].message.content):
-            raise ValueError(
-                f'Error calling OpenAI API or empty response="{response}".'
-            )
-
-        # Convert the content to an integer
-        try:
-            content = int(content.strip())
-        except Exception as e:
-            msg = f"Failed to convert OpenAI response '{content}' to integer: {e}"
-            logger.error(msg)
-            raise ValueError(msg)
-
-        # For tracking consumption we alre return the tokens used
-        classification = ClassificationResult(
-            result=content,
-            input_tokens=response.usage.prompt_tokens,
-            output_tokens=response.usage.completion_tokens,
-        )
-
-        return classification
 
     async def classify(
         self,
