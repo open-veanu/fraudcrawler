4b397083de4020f530dcdf65aeda246263acf1a7
chriguBERTO <christoph.jaeggli@gmail.com>
2026-04-11 22:24:17 +0200

    Looser UserInput TypeAlias

--- fraudcrawler/processing/base.py
+++ fraudcrawler/processing/base.py
@@ -13,7 +13,9 @@ logger = logging.getLogger(__name__)
 
 
 Context: TypeAlias = Dict[str, str]
-UserInputs: TypeAlias = Dict[str, List[str]]
+_JsonPrimitive: TypeAlias = str | int | float | bool | None
+_JsonValue: TypeAlias = _JsonPrimitive | List["_JsonValue"] | Dict[str, "_JsonValue"]
+UserInputs: TypeAlias = dict[str, _JsonValue]
 
 
 class ClassificationResult(BaseModel):
