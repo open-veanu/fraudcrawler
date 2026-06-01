4ff6302e21b87b96688cf6160181ba8c686dfaf8
chriguBERTO <christoph.jaeggli@gmail.com>
2026-06-01 15:33:49 +0200

    removed max_tokens entirely

--- fraudcrawler/processing/openai.py
+++ fraudcrawler/processing/openai.py
@@ -366,8 +366,6 @@ class OpenAIClassification(OpenAIWorkflow):
         which the classification should happen.
     """
 
-    _max_completion_tokens: int = 1
-
     def __init__(
         self,
         http_client: httpx.AsyncClient,
@@ -457,7 +455,6 @@ class OpenAIClassification(OpenAIWorkflow):
             product=product,
             system_prompt=self._system_prompt,
             user_prompt=user_prompt,
-            max_completion_tokens=self._max_completion_tokens,
         )
 
         if clfn.result not in self._allowed_classes:
